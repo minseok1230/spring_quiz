b660ab3f0838123b1b70c3bd8c86e48c2e4b4d72
test <test@test.com>
2023-06-15 18:41:36 +0900

    20230615 Spring DB연동 퀴즈

---
+++ src/main/java/com/quiz/lesson02/bo/StoreBO.java
@@ -0,0 +1,21 @@
+package com.quiz.lesson02.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.quiz.lesson02.dao.StoreMapper;
+import com.quiz.lesson02.domain.Store;
+
+@Service
+public class StoreBO {
+		
+		@Autowired
+		private StoreMapper storeMapper; //spring bean을 주입받는다. DI
+		
+		// output: storeList => 컨트롤러한테 돌려준다
+		public List<Store> getStoreList(){
+			List<Store> storeList = storeMapper.selectStoreList();
+			return storeList;
+		}
+}
