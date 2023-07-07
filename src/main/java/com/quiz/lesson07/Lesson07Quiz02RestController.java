19273271473cbf55d449603b0bea0f2a2d7cf890
test <test@test.com>
2023-07-07 16:02:12 +0900

    20230706

--- src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
@@ -26,38 +26,40 @@ public class Lesson07Quiz02RestController {
 		
 		@GetMapping("/2")
 		// http://localhost:8080/lesson07/quiz02/2
-		public List<AnnouncementEntity> quiz02(){
-			return announcementReporitory.findByEnterpriseId(2);
+		public List<AnnouncementEntity> quiz02_2(){
+			return announcementReporitory.findByEnterpriseId(1);
 		}
 		
 		@GetMapping("/3")
 		// http://localhost:8080/lesson07/quiz02/3
-		public List<AnnouncementEntity> quiz03(){
+		public List<AnnouncementEntity> quiz02_3(){
 			return announcementReporitory.findByPositionAndType("웹 back-end 개발자", "정규직");
 		}
 		
 		@GetMapping("/4")
 		// http://localhost:8080/lesson07/quiz02/4
-		public List<AnnouncementEntity> quiz04(){
+		public List<AnnouncementEntity> quiz02_4(){
+			// 정규직이거나 연봉이 9000 이상인 공고를 조회 
 			return announcementReporitory.findByTypeOrSalaryGreaterThanEqual("정규직", 9000);
 		}
 		
 		@GetMapping("/5")
 		// http://localhost:8080/lesson07/quiz02/5
-		public List<AnnouncementEntity> quiz05(){
+		public List<AnnouncementEntity> quiz02_5(){
 			return announcementReporitory.findTop3ByTypeOrderBySalaryDesc("계약직");
 		}
 		
 		@GetMapping("/6")
 		// http://localhost:8080/lesson07/quiz02/6
-		public List<AnnouncementEntity> quiz06(){
-			return announcementReporitory.findByRegionAndSalaryBetween("성남시 분당구", 7000,8500);
+		public List<AnnouncementEntity> quiz02_6(){
+			//성남시 분당구가 지역인 연봉 7000 이상 8500 이하인 공고를 조회하고 아래와 같이 출력하세요.
+			return announcementReporitory.findByRegionAndSalaryBetween("성남시 분당구", 7000, 8500);
 		}
 		
 		@GetMapping("/7")
 		// http://localhost:8080/lesson07/quiz02/7
-		public List<AnnouncementEntity> quiz07(){
-			return announcementReporitory.findBySalaryGreaterThanEqualAndTypeAndDeadlineAfterOrderBySalaryDesc(8100,"정규직","2026-04-10");
+		public List<AnnouncementEntity> quiz02_7(){
+			return announcementReporitory.findByDeadlineAfterAndSalaryGreaterThanEqualAndTypeAndOrderBySalaryDesc("2026-04-10", 8100,"정규직");
 		}
 		
 }
--- src/main/java/com/quiz/lesson07/dao/AnnouncementRepository.java
+++ src/main/java/com/quiz/lesson07/dao/AnnouncementRepository.java
@@ -1,9 +1,10 @@
 package com.quiz.lesson07.dao;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.quiz.lesson07.entity.AnnouncementEntity;
@@ -12,18 +13,25 @@ import com.quiz.lesson07.entity.AnnouncementEntity;
 public interface AnnouncementRepository extends JpaRepository<AnnouncementEntity, Integer>{
 	
 		//spring  data JPA
-	 	//2
+		
+		// JPQL -> Entity에 조회하는 개념
+	 	//quiz02_2
 		public List<AnnouncementEntity> findByEnterpriseId(int enterpriseId);
-		//3
+		//quiz02_3
 		public List<AnnouncementEntity> findByPositionAndType(String position, String type);
-		//4
+		//quiz02_4
 		public List<AnnouncementEntity> findByTypeOrSalaryGreaterThanEqual(String type, int salary);
-		//5
+		//quiz02_5
 		public List<AnnouncementEntity> findTop3ByTypeOrderBySalaryDesc(String type);
-		//6
+		//quiz02_6
 		public List<AnnouncementEntity> findByRegionAndSalaryBetween(String region, int n1, int n2);
-		//7
-		public List<AnnouncementEntity> findBySalaryGreaterThanEqualAndTypeAndDeadlineAfterOrderBySalaryDesc(int salary, String type, String deadline);
+		//quiz02_7 - native query => DB에 직접 조회(Mysql 쿼리문)
+		//마감일이 2026-04-10 이후이고 연봉이 8100 이상인 정규직 공고를 연봉 내림차순으로 조회하세요.
+		@Query(value = "select * from announcement where deadline > :deadline and salary >= :salary and type = :type order by salary desc", nativeQuery = true)
+		public List<AnnouncementEntity> findByDeadlineAfterAndSalaryGreaterThanEqualAndTypeAndOrderBySalaryDesc(
+				@Param("deadline") String deadline, 
+				@Param("salary") int salary, 
+				@Param("type") String type);
 		
 		
 		
